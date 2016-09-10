fd1b3a4e22d8270ac786d47c9382035432f75678
LFP6 <LFP6@users.noreply.github.com>
2016-09-09 21:29:36 -0400

    Fix for custom font colors not being displayed

--- js/chat.js
+++ js/chat.js
@@ -171,7 +171,7 @@
         var colors = ["#f39191", "#f39691", "#f39b91", "#f39f91", "#f3a491", "#f3a891", "#f3ad91", "#f3b191", "#f3b691", "#f3ba91", "#f3bf91", "#f3c491", "#f3c891", "#f3cd91", "#f3d191", "#f3d691", "#f3da91", "#f3df91", "#f3e491", "#f3e891", "#f3ed91", "#f3f191", "#f0f391", "#ebf391", "#e7f391", "#e2f391", "#ddf391", "#d9f391", "#d4f391", "#d0f391", "#cbf391", "#c7f391", "#c2f391", "#bef391", "#b9f391", "#b4f391", "#b0f391", "#abf391", "#a7f391", "#a2f391", "#9ef391", "#99f391", "#94f391", "#91f393", "#91f398", "#91f39c", "#91f3a1", "#91f3a5", "#91f3aa", "#91f3ae", "#91f3b3", "#91f3b7", "#91f3bc", "#f391ba", "#f391b6", "#f391b1", "#f391ad", "#f391a8", "#f391a4", "#f3919f", "#f3919b", "#f39196"];
         // Find escaped font tags, and replace them with a span and the hex color signified, if it's an action remove the tag
         // Unencoded, the regex would look like /<font color=(?:'|")?#([a-fA-F\d]{6})(?:'|")?>(.+)</font>/i
-        var customColored = data.replace(/&lt;font color=(?:&quot;|&#x27;)?#([a-fA-F\d]{6})(?:&quot;|&#x27;)?&gt;(.+)&lt;&#x2F;font&gt;/i, isAction ? '$2':'<span style="color:$1;">$2</span>')
+        var customColored = data.replace(/&lt;font color=(?:&quot;|&#x27;)?(#[a-fA-F\d]{6})(?:&quot;|&#x27;)?&gt;(.+)&lt;&#x2F;font&gt;/i, isAction ? '$2':'<span style="color:$1;">$2</span>')
                                 // TODO: Probably remove eventually, only needed for existing /me (once the Flash app is removed ACTION should be used)
                                 .replace(/&lt;I&gt;(.+)&lt;&#x2F;I&gt;/, '<span style="font-style: italic;">$1</span>');
         // Construct span, if it's an action omit the coloring
