f3827910de0ed576b570eddbf00f3d17d0941510
LFP6 <LFP6.01@gmail.com>
2017-06-02 13:15:59 +0000

    Use decodeURIComponent when getting username param from iframe

--- src/js/define-user.js
+++ src/js/define-user.js
@@ -4,9 +4,9 @@ var CHAT_CHANNEL = "global";
 location.search.slice(1).split("&").forEach(function(item){
     var param = item.split("=")[0];
     if (param == "name" || param == "uid") {
-        CURRENT_USER[param] = item.split("=")[1];
+        CURRENT_USER[param] = decodeURIComponent(item.split("=")[1]);
     } else if (param == "channel") {
-        CHAT_CHANNEL = item.split("=")[1];
+        CHAT_CHANNEL = decodeURIComponent(item.split("=")[1]);
     }
 });
 if (CURRENT_USER.name === undefined || CURRENT_USER.uid === undefined) {
