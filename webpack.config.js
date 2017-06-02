8c2eb7c56a7f7410ba87a25905831e1c5dfb4b28
LFP6 <LFP6.01@gmail.com>
2017-06-02 13:25:56 +0000

    Use API to get username on test page

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,14 @@ const developmentConfig = {
                     url:'https://www.eternagame.org/login/',
                     form: {name:req.body.user, pass:req.body.pass, type:'login'}
                 }, function(err,response,body){
-                    res.json(JSON.parse(body).data)
+                    data = JSON.parse(body).data;
+                    request.get({
+                        url: 'https://www.eternagame.org/get/',
+                        qs: {uid: data.uid}
+                    }, function(err, response, body) {
+                        data.name = JSON.parse(body).data.user.name;
+                        res.json(data);
+                    });
                 });
             });
         }
