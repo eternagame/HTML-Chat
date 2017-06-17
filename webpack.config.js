cfe69e7cb9bd98fd53358413e968d2d55c5f2dba
LFP6 <LFP6.01@gmail.com>
2017-06-17 19:55:30 +0000

    Fix getting username in wds auth: add missing query type user and don't attempt query if login failed

--- webpack.config.js
+++ webpack.config.js
@@ -62,13 +62,17 @@ const developmentConfig = {
                     form: {name:req.body.user, pass:req.body.pass, type:'login'}
                 }, function(err,response,body){
                     data = JSON.parse(body).data;
-                    request.get({
-                        url: 'https://www.eternagame.org/get/',
-                        qs: {uid: data.uid}
-                    }, function(err, response, body) {
-                        data.name = JSON.parse(body).data.user.name;
+                    if (data.success) {
+                        request.get({
+                            url: 'https://www.eternagame.org/get/',
+                            qs: {uid: data.uid, type: 'user'}
+                        }, function(err, response, body) {
+                            data.name = JSON.parse(body).data.user.name;
+                            res.json(data);
+                        });
+                    } else {
                         res.json(data);
-                    });
+                    }
                 });
             });
         }
