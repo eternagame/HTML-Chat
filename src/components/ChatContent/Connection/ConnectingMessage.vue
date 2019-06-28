711e0661710a9298e17f9b3cec83e81ceff6d4cc
guyguy2001 <guygeva2311@gmail.com>
2019-06-29 00:21:08 +0300

    Remove excess import

--- src/components/ChatContent/Connection/ConnectingMessage.vue
+++ src/components/ChatContent/Connection/ConnectingMessage.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script lang="ts">
-  import { Component, Prop } from 'vue-property-decorator';
+  import { Component } from 'vue-property-decorator';
   import Vue from '@/types/vue';
   import MessageComp from '@/components/ChatContent/Messages/Message.vue';
 
