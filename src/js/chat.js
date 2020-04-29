996d5c688d89c537f5b34dde2d3f4b9828ff3ed6
AlexHalb <46549413+AlexHalb@users.noreply.github.com>
2020-04-29 12:43:46 -0500

    Emoticons
    Added support for Unicode emoticons. Currently works with happy, sad, angry, surprised, and curious faces and thumbs up and thumbs down.

--- src/js/chat.js
+++ src/js/chat.js
@@ -303,7 +303,24 @@ function postMessage( raw_msg, isHistory ) {
     // Also, the `code` should be styled (ie have a background color and maybe a different text color), and it currently isn't.
     // Images are also disabled, as we currently don't have a way to format them properly.
     raw_msg = md.renderInline(raw_msg);
-
+    
+    //Emoticons
+    raw_msg = raw_msg.replace("?happy","&#128512"); //Replace ?happy and synonyms with smiling face emoji. Emoji depends on platform user is on
+    raw_msg = raw_msg.replace("?smile","&#128512");
+    raw_msg = raw_msg.replace("?smiling","&#128512");
+    raw_msg = raw_msg.replace("?sad","&#128546"); //Same as above, but for sad face 
+    raw_msg = raw_msg.replace("?crying","&#128546");
+    raw_msg = raw_msg.replace("?angry","&#128544"); //Angry face
+    raw_msg = raw_msg.replace("?mad","&#128544"); 
+    raw_msg = raw_msg.replace("?surprised","&#128558"); //Surprised face
+    raw_msg = raw_msg.replace("?shocked","&#128558");
+    raw_msg = raw_msg.replace("?shock","&#128558");
+    raw_msg = raw_msg.replace("?surpise","&#128558");
+    raw_msg = raw_msg.replace("?question","&#129300"); //Curious face
+    raw_msg = raw_msg.replace("?curious","&#129300");
+    raw_msg = raw_msg.replace("?wondering","&#129300");
+    raw_msg = raw_msg.replace("?thumbsup","&#128077"); //Thumbs up
+    raw_msg = raw_msg.replace("?thumbsup","&#128078"); //Thumbs down 
 
     // Don't show messages from user on ignore list
     if ( ignoredUsers.includes(name) ){return;}
