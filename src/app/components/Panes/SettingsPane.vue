7efe3debf404668487b8f0dda3d92b9ad27a2412
AlexHalb <46549413+AlexHalb@users.noreply.github.com>
2020-05-01 17:40:24 -0500

    Settings panel with ignored users list

---
+++ src/app/components/Panes/SettingsPane.vue
@@ -0,0 +1,95 @@
+<template>
+  <Pane ref="pane" :visibility="visibility">
+    <h1>Text Size</h1>
+    <input v-model="message" type=range min=10 max=22>
+    <span id='font-size-p'>{{text}}</span>
+    <p id='preview-p' :style='{ fontSize: fontSize + "px" }'>Preview</p>
+    <h1>Ignored Users</h1>
+    <ul>
+      <li v-for="user in ignoredUsers" :key="user.Username">
+        {{ user }}
+      </li>
+    </ul>
+    <template v-slot:footer>
+      <ConnectButton
+        v-if="!connectionData.firstConnection &&
+        !connectionData.connected"
+        class="connect-button"
+      />
+    </template>
+  </Pane>
+</template>
+
+<script lang="ts">
+  import {
+    Component, Watch, Prop, Vue,
+  } from 'vue-property-decorator';
+  import Pane from './Pane.vue';
+  import Username from '@/components/Messages/Username.vue';
+  import ConnectButton from '@/components/Connection/ConnectButton.vue';
+
+  @Component({
+    components: {
+      Username,
+      Pane,
+      ConnectButton,
+    },
+  })
+  export default class SettingsPane extends Vue {
+    @Prop({ required: true })
+    visibility!: boolean;
+
+    $refs!: {
+      pane: Pane;
+    };
+
+    @Prop({ required: true })
+    message!: '14';
+
+    get text() {
+      if (parseInt(this.message, 10) === 14) {
+        const newMessage = this.message.concat(' (default)');
+        return newMessage;
+      }
+      return this.message;
+    }
+
+    get fontSize() {
+      return this.message;
+    }
+
+    get connectedUsers() {
+      return this.$vxm.chat.connectedUsers;
+    }
+
+    get connectionData() {
+      return this.$vxm.chat.connectionData;
+    }
+
+    get ignoredUsers() {
+      return this.$vxm.chat.ignoredUsers;
+    }
+
+    @Watch('connectedUsers')
+    onContentChanged() {
+      this.$refs.pane.onContentChanged();
+    }
+
+    @Watch('connectionData.connected')
+    updateFooterHeight() {
+      this.$nextTick(this.$refs.pane.updateFooterHeight);
+    }
+  }
+</script>
+<style scoped>
+#font-size-p {
+  vertical-align: super;
+  margin-left:2px;
+}
+h1 {
+  margin:2px;
+}
+input {
+  margin:2px;
+}
+</style>
