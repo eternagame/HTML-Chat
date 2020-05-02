106f1b6f028ecf3e29db60de74068db43f525293
AlexHalb <46549413+AlexHalb@users.noreply.github.com>
2020-05-02 12:21:04 -0500

    Buttons are now working component

--- src/app/components/EmoticonButton.vue
+++ src/app/components/EmoticonButton.vue
@@ -1,5 +1,5 @@
 <template>
-<button class='emoticon-button' v-on:click="add()">
+<button class='emoticon-button' v-on:click="add()" :style="{ right: rightAmount + 'px'}">
   {{emote}}
 </button>
 </template>
@@ -14,8 +14,15 @@
     @Prop({ required: true })
     emoticon!: string;
 
+    @Prop({ required: true })
+    position!: Number;
+
     get emote() {
       return this.emoticon;
+    }
+
+    get rightAmount() {
+      return ((Number(this.position) - 1) * 20).toString();
     }
 
     add() {
@@ -29,7 +36,10 @@ button {
   position: absolute;
   bottom: 0px;
   right:0px;
-  padding:4px;
+  padding:3px;
   margin:5px;
+  border: solid 1px white;
+  border-radius:10%;
+  background-color:white;
 }
 </style>
