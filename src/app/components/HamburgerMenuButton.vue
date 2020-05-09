e5139d8f9852eb3e9a24203f187aacfbc4713790
AlexHalb <ahalbesleben@holyspirit-al.com>
2020-05-09 03:12:29 -0500

    Basic notification structure

--- src/app/components/HamburgerMenuButton.vue
+++ src/app/components/HamburgerMenuButton.vue
@@ -2,6 +2,7 @@
   <span
     class="hamburger"
     @click="$emit('input', !value)"
+    :class="{notified:notification}"
   />
 </template>
 
@@ -13,16 +14,25 @@
   export default class HamburgerMenuButton extends Vue {
     @Prop()
     value!: boolean;
+
+    @Prop()
+    notification!: boolean;
   }
 </script>
 <style scoped>
 .hamburger {
-  background-image: url("../assets/hamburger.png");
-  background-repeat: no-repeat;
-  background-size: 40% 40%;
-  background-position: center;
   display:inline-block;
   width:40px;
   height:40px;
+  background-image: url("../assets/hamburger.png");
+  background-repeat: no-repeat;
+  background-size: 50% 45%;
+  background-position: bottom 10px left 11px;
+  transition: background-image background-size 1s;
+}
+.notified {
+  background-image: url("../assets/hamburger-notification.png");
+  background-size: 62.5% 55%;
+  background-position:bottom 10px left 11px;
 }
 </style>
