a8d567a59013e1f147061431d48e66097b440cef
AlexHalb <ahalbesleben@holyspirit-al.com>
2020-06-22 10:28:54 -0500

    Chat positioned relative to edges when in-game

--- src/app/components/DraggableDiv.vue
+++ src/app/components/DraggableDiv.vue
@@ -24,6 +24,7 @@
     },
     props: {
       enabled: Boolean,
+      inGame: Boolean,
     },
     computed: {
       disabled: function disabled() {
@@ -66,19 +67,45 @@
       closeDragElement() {
         /* Chat position is stored relative to the center of the page - this is because
         of how the site is laid out. It means that the chat won't move if the browser resizes. */
+        /* If the chat is in-game, position is stored relative to edges of page */
         if (localStorage) {
-          // Sets new chat position in localStorage when drag is ended
-          let x = this.$refs.draggableContainer.style.left; // Gets current X and Y
-          let y = this.$refs.draggableContainer.style.top;
-          x = x.replace('px', '');
-          y = y.replace('px', '');
-          const point = [Number(x), Number(y)];
-          const midpointX = window.innerWidth / 2; // Gets center
-          const midpointY = window.innerHeight / 2;
-          const midpoint = [midpointX, midpointY];
-          // Gets chat's offset from the center
-          const offset = [midpoint[0] - point[0], midpoint[1] - point[1]];
-          localStorage.position = JSON.stringify(`${offset[0]} ${offset[1]}`); // Save to localStorage
+          if (this.inGame) {
+            const windowSize = [window.innerWidth, window.innerHeight];
+            const {
+              left, top, width, height,
+            } = this.$refs.draggableContainer.style;
+            const chatPosition = [Number(left.replace('px', '')), Number(top.replace('px', ''))];
+            const chatSize = [Number(width.replace('px', '')), Number(height.replace('px', ''))];
+            // Determines whether chat is placed WRT l/r, t/b by center
+            const center = [chatPosition[0] + chatSize[0] / 2, chatPosition[1] + chatSize[1] / 2];
+            const side = center[0] < windowSize[0] / 2 ? 'l' : 'r';
+            const side2 = center[1] < windowSize[1] / 2 ? 't' : 'b';
+            // Gets offsets from sides
+            let offset = chatPosition[0];
+            if (side === 'r') {
+              // Gets offset from right
+              offset = windowSize[0] - chatPosition[0] - chatSize[0];
+            }
+            let offset2 = chatPosition[1];
+            if (side2 === 'b') {
+              // Gets offset from bottom
+              offset2 = windowSize[1] - chatPosition[1] - chatSize[1];
+            }
+            localStorage.gamePosition = JSON.stringify([side, offset, side2, offset2]);
+          } else {
+            // Sets new chat position in localStorage when drag is ended
+            let x = this.$refs.draggableContainer.style.left; // Gets current X and Y
+            let y = this.$refs.draggableContainer.style.top;
+            x = x.replace('px', '');
+            y = y.replace('px', '');
+            const point = [Number(x), Number(y)];
+            const midpointX = window.innerWidth / 2; // Gets center
+            const midpointY = window.innerHeight / 2;
+            const midpoint = [midpointX, midpointY];
+            // Gets chat's offset from the center
+            const offset = [midpoint[0] - point[0], midpoint[1] - point[1]];
+            localStorage.position = JSON.stringify(`${offset[0]} ${offset[1]}`); // Save to localStorage
+          }
         }
         document.onmouseup = null;
         document.onmousemove = null;
