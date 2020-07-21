45e7a1cccf79ce747e788e6855a207f6aad5b2b1
AlexHalb <ahalbesleben@holyspirit-al.com>
2020-07-21 11:40:20 -0500

    Settings tooltip fixes

--- src/app/components/Slideout/Settings/SettingsTooltip.vue
+++ src/app/components/Slideout/Settings/SettingsTooltip.vue
@@ -1,7 +1,18 @@
 <template>
-  <span :title="text" class="settings-tooltip">◯
+  <span
+    class="settings-tooltip"
+    :id="`settings-tooltip-${text.split(' ').slice(0, 3).join('_')}...`"
+  ><pre class="d-inline"> </pre>
+    <b-tooltip
+      :target="`settings-tooltip-${text.split(' ').slice(0, 3).join('_')}...`"
+      variant="light"
+      triggers="hover focus"
+    >
+      {{ text }}
+    </b-tooltip>
   </span>
 </template>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script lang="ts">
   import { Component, Prop, Vue } from 'vue-property-decorator';
 
@@ -12,15 +23,20 @@
   }
 </script>
 <style>
-.settings-tooltip {
-  position: relative;
-  font-size:16px;
-}
 .settings-tooltip:before {
   content:"?";
-  position:absolute;
-  left: 5px;
-  top:2px;
   font-size: 12px;
+  line-height: 1;
+  width: 1rem;
+  height: 1rem;
+  position: absolute;
+  border-radius: 50%;
+  border: 1px solid white;
+  padding: 1px;
+  text-align: center;
+  display: inline-block;
+  margin: 4px;
+  cursor:pointer;
+  margin-left: 0px;
 }
 </style>
