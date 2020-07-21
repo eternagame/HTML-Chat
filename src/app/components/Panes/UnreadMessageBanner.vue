581004441089902b75f95399106b21f5064e8be7
AlexHalb <46549413+AlexHalb@users.noreply.github.com>
2020-07-21 10:09:54 -0500

    Update src/app/components/Panes/UnreadMessageBanner.vue
    Co-authored-by: Jonathan Romano <lfp6.01@gmail.com>

--- src/app/components/Panes/UnreadMessageBanner.vue
+++ src/app/components/Panes/UnreadMessageBanner.vue
@@ -11,7 +11,7 @@
   @Component
   export default class UnreadMessageBanner extends Vue {
     @Prop({ required: true })
-    messages !: number;
+    messages!: number;
   }
 </script>
 <style lang="scss">
