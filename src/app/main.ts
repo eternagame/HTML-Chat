1cc7c613fc82ad8ad59d3ada3e8e0853f8a87d22
Mark Hernandez <mark@lion-byte.com>
2026-02-25 18:32:30 -0600

    Remove vue-resource. Not used.

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,12 +1,10 @@
 import Vue from 'vue';
-import VueResource from 'vue-resource';
 import BootstrapVue from 'bootstrap-vue';
 import VModal from 'vue-js-modal';
 import App from './App.vue';
 
 Vue.use(VModal);
 Vue.use(BootstrapVue);
-Vue.use(VueResource);
 Vue.config.productionTip = false;
 
 // Vue.config.performance = true;
