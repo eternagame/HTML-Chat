70231565f2b9cb3d5615bf135e5826fa4980b4ee
Mark Hernandez <mark@lion-byte.com>
2026-03-18 21:08:19 -0500

    Add useMessageGroups composable

---
+++ src/composables/useMessageGroups.ts
@@ -0,0 +1,39 @@
+import type { Message } from '#models';
+import { parseNick } from '#utils';
+import { computed, type Ref } from 'vue';
+
+export interface MessageGroup extends Pick<Message, 'nick' | 'type'> {
+  id: string;
+  messages: Message[];
+}
+
+/**
+ * Group consecutive messages from same user.
+ * Does not group non-`privmsg` entries.
+ */
+export function useMessageGroups(messages: Ref<Message[]>) {
+  const messageGroups = computed(() => {
+    const groups: MessageGroup[] = [];
+    for (const message of messages.value) {
+      const prevGroup = groups.at(-1) ?? null;
+
+      if (
+        !prevGroup ||
+        message.type !== 'privmsg' ||
+        prevGroup.type !== 'privmsg' ||
+        parseNick(prevGroup.nick) !== parseNick(message.nick)
+      ) {
+        groups.push({
+          id: `group-${message.time}-${message.nick}`,
+          type: message.type,
+          messages: [message],
+          nick: message.nick,
+        });
+      } else {
+        prevGroup.messages.push(message);
+      }
+    }
+    return groups;
+  });
+  return { messageGroups };
+}
