454eeb41e2fe9a9e2fc8f6be2ff2b141a963488b
Mark Hernandez <mark@lion-byte.com>
2026-03-30 20:45:09 -0500

    Add Report model

---
+++ src/models/report.model.ts
@@ -0,0 +1,9 @@
+import type { Message } from './message.model';
+import type { User } from './user.model';
+
+export interface Report {
+  targetUser: Pick<User, 'username' | 'uid'>;
+  targetMessage?: Pick<Message, 'id' | 'time' | 'message' | 'type' | 'target' | 'nick'>;
+  reporter: Pick<User, 'username' | 'uid'>;
+  reportComments: string;
+}
