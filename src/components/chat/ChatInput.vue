664f793d30f9fb67f09c737f10c37abe1c33fdd9
Mark Hernandez <mark@lion-byte.com>
2026-04-17 21:03:22 -0500

    Connect keybind to input

--- src/components/chat/ChatInput.vue
+++ src/components/chat/ChatInput.vue
@@ -6,14 +6,16 @@
     </div>
 
     <BForm @submit.prevent="onSubmit">
-      <ChatToolbar :aria-controls="inputId" />
+      <ChatToolbar :aria-controls="inputId" @format="onFormat" />
       <BFormGroup label="Chat message" label-visually-hidden>
         <BInputGroup size="lg">
           <BFormInput
             :id="inputId"
+            ref="chat-input"
             v-model="inputBuffer"
             autocomplete="off"
             :placeholder="placeholder"
+            @keydown="onKeydown"
           />
           <BButton type="submit" variant="primary" class="px-4" :disabled="!inputBuffer.trim()"
             >Send</BButton
@@ -28,15 +30,67 @@
   import MessageContent from '#components/message/MessageContent.vue';
   import { useChannelStore, useChatStore } from '#stores';
   import { BButton, BForm, BFormGroup, BFormInput, BInputGroup } from 'bootstrap-vue-next';
-  import { computed, ref, useId } from 'vue';
+  import { computed, nextTick, ref, useId, useTemplateRef } from 'vue';
   import ChatToolbar from './ChatToolbar.vue';
+  import type { MdFormat } from '#models';
+  import { toggleMDFormat } from '#utils/md-format.util.ts';
 
   const channel = useChannelStore();
   const chat = useChatStore();
 
   const inputId = useId();
+  const inputRef = useTemplateRef('chat-input');
   const inputBuffer = ref('');
   const placeholder = computed(() => `Message ${channel.currentChannel?.displayName ?? ''}`);
+
+  function onFormat(format: MdFormat) {
+    if (!inputRef.value?.element) {
+      return;
+    }
+    const input = inputRef.value.element;
+    const { selectionDirection, selectionStart, selectionEnd } = input;
+    const start = typeof selectionStart === 'number' ? selectionStart : inputBuffer.value.length;
+    const end = typeof selectionEnd === 'number' ? selectionEnd : start;
+    const output = toggleMDFormat({
+      text: inputBuffer.value,
+      selectionRange: [start, end],
+      format,
+    });
+    inputBuffer.value = output.text;
+    nextTick(() => {
+      input.focus();
+      input.setSelectionRange(
+        output.selectionRange[0],
+        output.selectionRange[1],
+        selectionDirection ?? undefined,
+      );
+    });
+  }
+
+  function onKeydown(event: KeyboardEvent) {
+    if (!(event.ctrlKey || event.metaKey)) {
+      return;
+    }
+
+    switch (event.key.toLocaleLowerCase()) {
+      case 'b':
+        event.preventDefault();
+        onFormat('Bold');
+        break;
+      case 'u':
+        event.preventDefault();
+        onFormat('Underline');
+        break;
+      case 'i':
+        event.preventDefault();
+        onFormat('Italics');
+        break;
+      case 'k':
+        event.preventDefault();
+        onFormat('Link');
+        break;
+    }
+  }
 
   function onSubmit() {
     const text = inputBuffer.value.trim();
--- src/utils/md-format.util.ts
+++ src/utils/md-format.util.ts
@@ -1,10 +1,12 @@
+import type { MdFormat } from '#models';
+import log from 'loglevel';
+
 type Range = [start: number, end: number];
 
 interface ToggleMdFormatInput {
   text: string;
   selectionRange: Range;
-  prefix: string;
-  suffix?: string;
+  format: MdFormat;
 }
 
 interface ToggleMDFormatResult {
@@ -12,29 +14,75 @@ interface ToggleMDFormatResult {
   selectionRange: Range;
 }
 
+function getFormatCharacters(format: MdFormat): [prefix: string, suffix: string] {
+  switch (format) {
+    case 'Bold':
+      return ['**', '**'];
+    case 'Underline':
+      return ['_', '_'];
+    case 'Italics':
+      return ['*', '*'];
+    case 'Strikethrough':
+      return ['~~', '~~'];
+    case 'Blockquote':
+      return ['> ', ''];
+    case 'Code':
+      return ['`', '`'];
+    case 'Link':
+      return ['[', ']()'];
+    case 'Highlight':
+      return ['|', '|'];
+    default:
+      log.warn('Unhandled format:', format);
+      return ['', ''];
+  }
+}
+
+/**
+ * Selects current word if there's no text selected
+ */
+function getTargetRange(text: string, [start, end]: Range): Range {
+  if (start !== end) {
+    return [start, end];
+  }
+  const left = text.slice(0, start).match(/([^\s*~_`|]+)$/);
+  const right = text.slice(start).match(/^([^\s*~_`|]+)/);
+  return [left ? start - left[0].length : start, right ? start + right[0].length : start];
+}
+
 export function toggleMDFormat({
   text,
-  selectionRange: [start, end],
-  prefix,
-  suffix = '',
+  selectionRange,
+  format,
 }: ToggleMdFormatInput): ToggleMDFormatResult {
-  const selectedText = text.substring(start, end);
+  const [prefix, suffix] = getFormatCharacters(format);
+  const isSelected = selectionRange[0] !== selectionRange[1];
+  const [start, end] = getTargetRange(text, selectionRange);
+
   const before = text.substring(0, start);
+  const targetText = text.substring(start, end);
   const after = text.substring(end);
 
-  const isWrapped = before.endsWith(prefix) && after.startsWith(suffix);
-
-  if (isWrapped && (prefix || suffix)) {
+  const hasPrefix = prefix && before.endsWith(prefix);
+  if (hasPrefix) {
     // Remove format
+    const hasSuffix = suffix && after.startsWith(suffix);
+    const newText =
+      before.slice(0, -prefix.length) + targetText + after.slice(hasSuffix ? suffix.length : 0);
+
+    const newStart = (isSelected ? start : selectionRange[0]) - prefix.length;
+    const newEnd = (isSelected ? end : selectionRange[0]) - prefix.length;
     return {
-      text: before.slice(0, -prefix.length) + selectedText + after.slice(suffix.length),
-      selectionRange: [start - prefix.length, end - prefix.length],
+      text: newText,
+      selectionRange: [newStart, newEnd],
     };
   } else {
     // Add format
+    const newStart = (isSelected ? start : selectionRange[0]) + prefix.length;
+    const newEnd = (isSelected ? end : selectionRange[0]) + prefix.length;
     return {
-      text: before + prefix + selectedText + suffix + after,
-      selectionRange: [start + prefix.length, end + prefix.length],
+      text: before + prefix + targetText + suffix + after,
+      selectionRange: [newStart, newEnd],
     };
   }
 }
